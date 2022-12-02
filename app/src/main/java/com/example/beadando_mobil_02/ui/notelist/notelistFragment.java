2e4cf41d8f0d3bd2a4f0389486eef8d12a6036de
vtian95 <101476410+vtian95@users.noreply.github.com>
2022-12-02 22:05:11 +0100

    Listás megjelenítés kialakítása, egyéb csiszolgatások

--- app/src/main/java/com/example/beadando_mobil_02/ui/notelist/notelistFragment.java
+++ app/src/main/java/com/example/beadando_mobil_02/ui/notelist/notelistFragment.java
@@ -3,12 +3,18 @@ package com.example.beadando_mobil_02.ui.notelist;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.beadando_mobil_02.R;
+import com.example.beadando_mobil_02.data.NotebookManager;
+import com.example.beadando_mobil_02.domain.Notebook;
+
+import java.util.List;
 
 
 public class notelistFragment extends Fragment {
@@ -20,6 +26,22 @@ public class notelistFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_notelist, container, false);
+        //adathalmaz
+        List<Notebook> notebookList = NotebookManager.loadMockData();
+
+        RecyclerView noterecyclerView = view.findViewById(R.id.Notebookrecycleview);
+
+        notelistadapter adapter = new notelistadapter(notebookList);
+        //egy oszlopos megjelenítés
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(
+                getActivity());
+
+        noterecyclerView.setLayoutManager(layoutManager);
+        noterecyclerView.setAdapter(adapter);
+
+
+
+
         return view;
 
     }
